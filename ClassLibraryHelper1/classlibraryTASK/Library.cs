a45e740e7198b08cfa1e042aba9e70eefb9a7a48
NIhat <135535115+Nihatgunay@users.noreply.github.com>
2024-06-05 02:14:15 +0400

    Add files via upload

---
+++ ClassLibraryHelper1/classlibraryTASK/Library.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace classlibraryTASK
+{
+    public class Library
+    {
+        List<Book> _books = new List<Book>();
+        public Book this[int index]
+        {
+            get { return _books[index]; }
+        }
+
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+        public Book FindAllBooksByName(string value)
+        {
+            List<Book> _newbooks = new List<Book>(0);
+            foreach (var book in _books)
+            {
+                if (book.Name == value)
+                {
+                    return book;
+                }
+            }
+            return null;
+        }
+        public Book FindBookByCode(string code)
+        {
+            foreach (var book in _books)
+            {
+                if (book.Code == code)
+                    return book;                
+            }
+            return null;
+        }
+        public Book RemoveAllBooksByName(string name)
+        {
+            foreach (var item in _books)
+            {
+                if (item.Name.Trim().ToLower() == name.Trim().ToLower())
+                {
+                    _books.Remove(item);
+                }
+            }
+            return null; ;
+            
+        }
+        public Book SearchBooks(string value)
+        {
+            foreach (var item in _books)
+            {
+                if (item.Code.Trim().ToLower() == value.Trim().ToLower() || 
+                    item.Name.Trim().ToLower() == value.Trim().ToLower() || 
+                    item.AuthorName.Trim().ToLower() == value.Trim().ToLower() || 
+                    item.PageCount == int.Parse(value))
+                {
+                    Console.WriteLine($"Name - {item.Name}  AuthorName - {item.AuthorName}  " +
+                        $"Code - {item.Code}  Pagecount - {item.PageCount}");
+                }
+            }
+            return null;
+        }
+        //public Book FindAllBooksByPageCountRange(int minpagecount, int maxpagecount)
+        //{
+        //    if (minpagecount < 0 || minpagecount > maxpagecount)
+        //    {
+        //        Console.WriteLine("Invalid min max page count");
+        //    }
+
+        //    return null;
+        //}
+        public Book RemoveBookByCode(string value)
+        {
+            foreach (var book in _books)
+            {
+                if (book.Code == value)
+                    _books.Remove(book);
+            }
+            return null;
+        }
+        
+    }
+}
---
+++ ClassLibraryHelper1/classlibraryTASK/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ClassLibraryHelper1;
+
+namespace classlibraryTASK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper helper = new Helper();
+            Library library = new Library();
+            Book book = new Book(); 
+
+            Book book1 = new Book();
+            book1.Name = "Programming";
+            book1.AuthorName = "John Wick";
+            book1.PageCount = 10;
+            book1.Code = Helper.CreateBookCode("Programming", 1);           
+
+            Book book2 = new Book();
+            book2.Name = "Design";
+            book2.AuthorName = "John Wash";
+            book2.PageCount = 100;
+            book2.Code = Helper.CreateBookCode("Design", 2); 
+            
+            library.AddBook(book1);
+            library.AddBook(book2);
+
+            //library.RemoveAllBooksByName(book1.Name);
+            library.FindAllBooksByName("John");
+            //book1.ShowInfo();
+            //book2.ShowInfo();
+        }
+    }
+}
